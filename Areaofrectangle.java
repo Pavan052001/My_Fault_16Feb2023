a0d5c9736891bf47bbdb8967fd159d338d3c0dc5
Sarveshbagde <sarveshbagde@gmail.com>
2023-02-16 22:02:19 +0530

    Finding Area of circle

---
+++ Areaofrectangle.java
@@ -0,0 +1,17 @@
+public class Areaofrectangle {
+    public static void main(String[] args) {
+
+        int height =7;
+        int width = 5;
+        int p;
+        int Areaofrectangle;
+
+        p=2*(height+width);
+        System.out.println("perimeter of rectangle ="+p);
+
+        Areaofrectangle= height*width;
+
+        System.out.println("Area of rectangle ="+Areaofrectangle);
+
+    }
+}
