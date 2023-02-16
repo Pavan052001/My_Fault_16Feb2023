2b25dae4de72bf17a02b09e6dcdddea1f3f21d85
Sarveshbagde <sarveshbagde@gmail.com>
2023-02-16 21:59:59 +0530

    Finding area of circle

---
+++ AreaOfcircle.java
@@ -0,0 +1,19 @@
+public class AreaOfcircle {
+    public static void main(String[] args) {
+
+        double pia =3.14;
+        int r=7;
+        double areaofcircle;
+        double p;
+
+        areaofcircle=3.14*(7*7);
+        System.out.println("area of circle is ="+areaofcircle);
+
+        p=2*3.14*r;
+        System.out.println("perimeter of circle = "+p);
+
+
+
+
+    }
+}
